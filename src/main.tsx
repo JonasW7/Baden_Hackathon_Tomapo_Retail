614e682b06d47f237dab6a3403437e337a14a5f2
Jonas Wildi <jonas@tie-international.com>
2025-11-26 16:52:26 +0100

    comments, darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,18 @@ import router from "@/router/router";
 import queryClient from "@/query/queryClient";
 import "@/assets/styles/globals.css";
 import "@/i18n/i18n";
+import { ThemeProvider } from "@/context/themeProvider";
 
 createRoot(document.getElementById("root")!).render(
+  // default wrapper from React for development mode
   <StrictMode>
+    {/* tanstack provider */}
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      {/* shadcn theme provider */}
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* react router provider */}
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>
 );
